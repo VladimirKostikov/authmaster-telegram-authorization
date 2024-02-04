9793af9e648344445910dd10467399100d66781e
Vladimir Kostikov <86430748+VladimirKostikov@users.noreply.github.com>
2024-02-04 16:27:43 +0300

    Laravel installed

--- bot/src/main.py
+++ bot/src/main.py
@@ -3,8 +3,16 @@ from modules import db
 import requests
 import telebot;
 
-bot = telebot.TeleBot('%ваш токен%');
+bot = telebot.TeleBot(config.token);
 
-def main():
-    obj = db.Database()
-    obj.log()
+@bot.message_handler(content_types=['text'])
+
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, message.from_user.id)
+
+bot.polling(none_stop=True, interval=0)
--- bot/src/modules/config.py
+++ bot/src/modules/config.py
@@ -8,3 +8,7 @@ hostname     =  config.get('db', 'hostname')
 database     =  config.get('db', 'database')
 user         =  config.get('db', 'user')
 password     =  config.get('db', 'password')
+
+config.read('config/tg.ini')
+ 
+token        =  config.get('tg', 'token')
